e1cb234b58a3adf807409759cee030dc5153ce49
oussamaitbs <oussama.ghariani@itbs.tn>
2022-09-14 23:29:18 +0200

    last commit

--- src/main/java/com/example/demo/Controller/ScriptsController.java
+++ src/main/java/com/example/demo/Controller/ScriptsController.java
@@ -1,16 +1,13 @@
 package com.example.demo.Controller;
 
+import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
+import com.example.demo.Entity.TestQA;
+import com.example.demo.Repository.TestQARepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.example.demo.Entity.Scripts;
 import com.example.demo.Repository.ScriptsRepository;
@@ -19,13 +16,31 @@ import com.example.demo.Services.ScriptsService;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
+@RequestMapping("/api/script")
 public class ScriptsController {
 	@Autowired
 	 private ScriptsService scriptsService;
 	@Autowired
 	private ScriptsRepository scriptsRepo;
 
+	@Autowired
+	private TestQARepository testQARepository;
 
+
+	@PutMapping("/updatetest/{id}")
+	public TestQA updateTest(@RequestBody Date date, @PathVariable("id") Long id){
+		TestQA test =testQARepository.findById(id).orElse(null);;
+		test.setDatefinScript(date);
+		testQARepository.save(test);
+		return test;
+	}
+	@PutMapping("/updatescripts/{id}")
+	public Scripts updateScripts(@RequestBody Date date, @PathVariable("id") Long id){
+		Scripts scripts =scriptsRepo.findById(id).orElse(null);;
+		 scripts.setDatefinScript(date);
+		scriptsRepo.save(scripts);
+		return scripts;
+	}
 	    public ScriptsController(ScriptsService scriptsService) {
 	        this.scriptsService = scriptsService;
 	    }
